37247afc380b321c222341f7d5235e9fd018b3fc
JJCONWAY23 <jacob.peat01@gmail.com>
2023-09-25 16:40:55 +0100

    complete function that displays message to frontend

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import { messages } from "./extra_files/messages.js";
 
 const newMsgBtn = document.querySelector(".new-message-btn");
+
+
 const getRandomMessage = () => {
     const randomNumber = Math.floor(Math.random() * messages.length);
     const author = messages[randomNumber].author;
@@ -11,3 +13,24 @@ const getRandomMessage = () => {
     }
 }
 
+const displayMessage = () => {
+    // getting ref to dom elements
+    const generatedMessage = document.querySelector('.generated-message');
+    const author = document.querySelector('.author');
+    const startingMessage = document.querySelector('.starting-message');
+
+    // getting result from function call
+    const randomMessage = getRandomMessage();
+
+    // setting new values to html
+    author.innerHTML = `~ ${randomMessage.author}`;
+    generatedMessage.innerHTML = randomMessage.quote;
+
+    //changing display values of dom elements.
+    startingMessage.style.display = 'none';
+    generatedMessage.style.display = 'block';
+    author.style.display = 'block';
+
+}
+
+newMsgBtn.addEventListener('click', displayMessage)
